c869dcebb60c7fd4aeb4dfa65d6a57103282f174
Thiago Andrade <thiagoh@gmail.com>
2015-11-02 17:32:46 -0300

    plenty of javascript improvements

--- app.js
+++ app.js
@@ -1,62 +1,75 @@
 (function($) {
-	
-	'use strict';
 
-	var data = [
-		'thiago andrade', 'bruno henrique',
-		'walmor cesar', 'breno nunes',
-		'd\'alia', 'toto-tata',
-		'jogo#velha', 'email@email.com',
-		'antonio braga', 'carlos leite',
-		'paulo amorim', 'raphael praxedes',
-		'thiago souza', 'thiago leite',
-		'thiago cavalcanti', 'thiago ferreira',
-		'thiago braga', 'thiago castro',
-		'thiago lima', 'thiago aragão',
-		'thiago alencar', 'thiago assis',
-		'hugo fernando', 'danilo lima',
-		'ronaldo nobrega', 'kleber xavier',
-		'andré barros', 'roberta almeida'
-	];
+	$(function() {
 
-	$('input[flexcomplete]').flexcomplete({
-		width: 300,
-		showTooltip: true,
-		data: data,
-		onSelect: function(val, input) {
+		var data = [
+				'thiago andrade', 'bruno henrique',
+				'walmor cesar', 'breno nunes',
+				'd\'alia', 'toto-tata',
+				'jogo#velha', 'email@email.com',
+				'antonio braga', 'carlos leite',
+				'paulo amorim', 'raphael praxedes',
+				'thiago souza', 'thiago leite',
+				'thiago cavalcanti', 'thiago ferreira',
+				'thiago braga', 'thiago castro',
+				'thiago lima', 'thiago aragão',
+				'thiago alencar', 'thiago assis',
+				'hugo fernando', 'danilo lima',
+				'ronaldo nobrega', 'kleber xavier',
+				'andré barros', 'roberta almeida'
+			],
+			remove = function(event) {
 
-			var self = this;
+				$(this).parent('.item').remove();
+			},
+			instance = $('input[flexcomplete]').flexcomplete({
+				width: 300,
+				showTooltip: true,
+				data: data,
+				onSelect: function(val, input) {
 
-			var item = $("<span class='item'>" + val + "<span class='remove-item'>x</span></span>")
-				.find('.remove-item').click(function() {
-					item.remove();
-				}).end();
+					$("<span class='item'>" + val + "<span class='remove-item'>x</span></span>")
+						.on('click', '.remove-item', remove).appendTo($('#container'));
 
-			$('#container').append(item);
+					input.value = '';
+				},
+				getLine: function(line) {
+					return line;
+				},
+				filter: function(line, input) {
+					input = $(input);
+					var re = new RegExp(input.val().replace($.fn.flexcomplete.reReplace, ''), "i");
+					return $(line).filter(function(i) {
+						return re.test(line[i].replace($.fn.flexcomplete.reReplace, ''));
+					});
+				}
+			}),
+			updateData = function() {
 
-			input.value = '';
-		},
-		getLine: function(data) {
-			return data;
-		},
-		filter: function(data, input) {
-			input = $(input);
-			var re = new RegExp(input.val().replace($.fn.flexcomplete.reReplace, ''), "i");
-			return $(data).filter(function(i) {
-				return re.test(data[i].replace($.fn.flexcomplete.reReplace, ''));
-			});
-		}
+				var dataContainer = $('#idDataContainer').empty();
+				var arr = instance.flexcomplete('staticData');
+				for (var i = 0; i < arr.length; i++) {
+					dataContainer.append('<div>' + arr[i] + '</div>');
+				}
+			};
+
+		$('#idAddDataButton').click(function() {
+
+			var v = $('#idAddData').val();
+
+			if ($.trim(v) === '') {
+				return;
+			}
+
+			$('#idAddData').val('');
+			updateData();
+		});
+
+		updateData();
+		$('#textbox').click(function() {
+			$('#id').focus();
+		});
+
 	});
 
-	var dataContainer = $('#idDataContainer').empty();
-
-	var arr = instance.flexcomplete('staticData');
-	for (var i = 0; i < arr.length; i++) {
-		dataContainer.append('<div>' + arr[i] + '</div>');
-	}
-
-	$('#textbox').click(function() {
-		$('#id').focus();
-	});
-	
 }(jQuery));
